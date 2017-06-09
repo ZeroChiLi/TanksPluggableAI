6ba73ab096964c9213ffa18bfe9ec97b8893c2b7
ChiLi_Lin <904981663@163.com>
2017-06-09 20:23:24 +0800

    Study Pluggable AI

---
+++ Assets/PluggableAI/Scripts/StateController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Complete;
+
+public class StateController : MonoBehaviour {
+
+
+	public EnemyStats enemyStats;
+	public Transform eyes;
+
+
+	[HideInInspector] public NavMeshAgent navMeshAgent;
+	[HideInInspector] public Complete.TankShooting tankShooting;
+	[HideInInspector] public List<Transform> wayPointList;
+
+	private bool aiActive;
+
+
+	void Awake () 
+	{
+		tankShooting = GetComponent<Complete.TankShooting> ();
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+
+	public void SetupAI(bool aiActivationFromTankManager, List<Transform> wayPointsFromTankManager)
+	{
+		wayPointList = wayPointsFromTankManager;
+		aiActive = aiActivationFromTankManager;
+		if (aiActive) 
+		{
+			navMeshAgent.enabled = true;
+		} else 
+		{
+			navMeshAgent.enabled = false;
+		}
+	}
+
+}
