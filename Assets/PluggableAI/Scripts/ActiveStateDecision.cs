1a5353bef093b31688fc8bd3b852a2fe47a785bd
ChiLi_Lin <904981663@163.com>
2017-06-10 10:37:59 +0800

    Add ChaseAction And ActiveStateDecition

---
+++ Assets/PluggableAI/Scripts/ActiveStateDecision.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "PluggableAI/Decisions/ActiveState")]
+public class ActiveStateDecision : Decision 
+{
+    public override bool Decide(StateController controller)
+    {
+        bool chaseTargetIsActive = controller.chaseTarget.gameObject.activeSelf;
+        return chaseTargetIsActive;
+    }
+
+}
---
+++ Assets/PluggableAI/Scripts/ChaseAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Chase")]
+public class ChaseAction : Action
+{
+    public override void Act(StateController controller)
+    {
+        Chase(controller);
+    }
+
+    private void Chase(StateController controller)
+    {
+        controller.navMeshAgent.destination = controller.chaseTarget.position;
+        controller.navMeshAgent.isStopped = false;
+    }
+
+}
